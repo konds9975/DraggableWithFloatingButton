a4ddba27a489029adf0512cb412a2e34eaf64d2a
Inkswipe <inkswipe@inkswipes-mac-mini.local>
2018-07-20 17:14:58 +0530

    Initial Commit

---
+++ DraggableWithFloatingButtonKonds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DraggableWithFloatingButtonKonds
+//
+//  Created by Inkswipe on 7/20/18.
+//  Copyright © 2018 Fortune4 Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
