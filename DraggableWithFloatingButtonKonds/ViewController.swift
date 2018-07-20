dea4b6fc90bf7f0b31b758a5e1d7fe03cc25b8ca
Inkswipe <inkswipe@inkswipes-mac-mini.local>
2018-07-20 17:27:12 +0530

    initialCommet

--- DraggableWithFloatingButtonKonds/ViewController.swift
+++ DraggableWithFloatingButtonKonds/ViewController.swift
@@ -12,14 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.addButton()
+     }
+
+    func addButton()  {
+        
+        var addButton: ActionButton!
+        let addInhaler = UIImage(named: "Add-inhalers.png")!
+        let addInhalerBtn = ActionButtonItem(title: "Add", image: addInhaler)
+        addInhalerBtn.action = { item in print("Btn Click...")
+            if addButton.active {
+                addButton.toggle()
+            }
+         }
+        addButton = ActionButton(attachedToView: self.view, items: [addInhalerBtn],bottomPadding:45)
+        
+        addButton.action = { button in button.toggleMenu() }
+        addButton.setTitle("+", forState: UIControlState())
+        
+        addButton.backgroundColor = UIColor(red: 136.0/255.0, green: 204.0/255.0, blue: 92.0/255.0, alpha:1.0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
